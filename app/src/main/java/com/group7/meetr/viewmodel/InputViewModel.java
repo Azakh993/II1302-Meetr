0073285053926b0bd544ab173be632ffabd5eea0
elibes <elibes@proton.me>
2023-04-27 14:46:52 +0200

    refactored user proximity code (added files)

---
+++ app/src/main/java/com/group7/meetr/viewmodel/InputViewModel.java
@@ -0,0 +1,12 @@
+package com.group7.meetr.viewmodel;
+
+import com.group7.meetr.data.remote.SessionHandler;
+
+public class InputViewModel {
+
+    private SessionHandler sessionHandler = new SessionHandler();
+    public void receiveProximityInput(long timestamp){
+        String sessionId = "7";
+        sessionHandler.sendProximityData(sessionId, true, timestamp);
+    }
+}
