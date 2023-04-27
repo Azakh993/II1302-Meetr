d04cf1088ec0ab534f0f5631409aefc3fe2bbca8
peterfann <43177628+peterfann@users.noreply.github.com>
2023-04-27 16:55:57 +0200

    fixed talking activity

--- app/src/main/java/com/group7/meetr/activity/InMeetingActivity.java
+++ app/src/main/java/com/group7/meetr/activity/InMeetingActivity.java
@@ -3,6 +3,7 @@ package com.group7.meetr.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.group7.meetr.R;
@@ -17,7 +18,17 @@ public class InMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant_view);
+        Button botton = findViewById(R.id.buttonJoin);
 
+        botton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                //TODO: Make participants activity and replace second variable here.
+                intent = new Intent(InMeetingActivity.this, TalkingActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
