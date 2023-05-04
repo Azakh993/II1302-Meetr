29b62a5589a9de189214343de0c7dca77ec8ee30
elibes <elibes@proton.me>
2023-05-04 14:31:56 +0200

    updated the intervals for sensor requests functionality

--- app/src/main/java/com/group7/meetr/viewmodel/InputViewModel.java
+++ app/src/main/java/com/group7/meetr/viewmodel/InputViewModel.java
@@ -5,7 +5,16 @@ import com.group7.meetr.data.remote.SessionHandler;
 public class InputViewModel {
 
     private SessionHandler sessionHandler = new SessionHandler();
+    
+    private static final long REQUEST_DEBOUNCE_INTERVAL = 1000; // 1 second
+    private long lastRequestTime = 0;
+
     public void receiveProximityInput(long timestamp){
-        sessionHandler.sendProximityData("7",timestamp);
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime - lastRequestTime > REQUEST_DEBOUNCE_INTERVAL) {
+            sessionHandler.sendProximityData("7",timestamp);
+            lastRequestTime = currentTime;
+        }
     }
 }
