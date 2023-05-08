049a783d5eb18457675c1af9bb9858495b351869
Azmeer K <azmeer.khawaja@outlook.com>
2023-05-08 22:27:36 +0200

    Moved code from FirebaseFunctionsManager to class. Reduced visibility of these for better encapsulation.

--- app/src/main/java/com/group7/meetr/data/remote/SessionHandler.java
+++ app/src/main/java/com/group7/meetr/data/remote/SessionHandler.java
@@ -1,30 +1,126 @@
 package com.group7.meetr.data.remote;
 
+import static com.group7.meetr.data.remote.UtilFunctions.anyFunction;
+import static com.group7.meetr.data.remote.UtilFunctions.fFunctions;
+
 import android.util.Log;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
 import com.group7.meetr.viewmodel.LoginPageViewModel;
-import com.group7.meetr.data.model.Participant;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SessionHandler {
-    private static DatabaseReference mDatabase;
 
     /**
-     * Class constructor. Initializes database reference.
+     * Private helper function to be run and awaited results.
+     * @param input object to input
+     * @return returns the task that hasnt finished yet.
      */
+    private static Task<Map<String, Object>> newMeeting(Map<String, Object> input) {
+        // Create the arguments to the callable function.
 
-    public SessionHandler() {
-        mDatabase = FirebaseDatabase.getInstance("https://meetr-android-default-rtdb.europe-west1.firebasedatabase.app/").getReference("/Sessions/");
+        input.put("push", true);
+        input.put("uid", LoginPageViewModel.getCurrentUser().getUid());
+
+        return fFunctions
+                .getHttpsCallable("newMeeting")
+                .call(input)
+                .continueWith(new Continuation<HttpsCallableResult, Map<String, Object>>() {
+                    @Override
+                    public Map<String, Object> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        Log.d("FFunctionsManager:newMeeting","Task successful?...");
+
+                        Map<String, Object> result = (Map<String, Object>)  task.getResult().getData();
+                        return (Map<String, Object>) result;
+                    }
+                });
     }
+
+    /**
+     * Call a new meeting using the FirebaseFunctions.
+     * @param moderatorName whats the name of the moderator.
+     * @param meetingID the meetingID to use to create.
+     * @return the meeting id as an ínteger. Should defintely be string...
+     */
+    private static String callNewMeeting(String moderatorName, String meetingID) {
+        if(fFunctions == null)
+            fFunctions = FirebaseFunctions.getInstance("europe-west1");
+
+        //{moderator: moderatorUserName<String>, meetingID: meetingID<String>}
+        Map<String, Object> sendInfo = new HashMap<>();
+        final String[] result = {null};
+        sendInfo.put("moderator", moderatorName);
+        sendInfo.put("mID", meetingID);
+        newMeeting(sendInfo).addOnCompleteListener(new OnCompleteListener<Map<String, Object>>() {
+            @Override
+            public void onComplete(@NonNull Task<Map<String, Object>> task) {
+                if (!task.isSuccessful()) {
+                    Exception e = task.getException();
+                    if (e instanceof FirebaseFunctionsException) {
+                        FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                        FirebaseFunctionsException.Code code = ffe.getCode();
+                        Object details = ffe.getDetails();
+                    }
+                }
+                else
+                {
+                    result[0] = String.valueOf(task.getResult().get("sid"));
+                    Log.d("fornite",(task.getResult().get("meetingID").toString()));
+                }
+
+            }
+        });
+        return result[0];
+    }
+
+
+    /** Joins a meeting using backend.
+     * @param meetingID meetingid to join
+     */
+    private static void callJoinMeeting(String meetingID, String name){
+        if(fFunctions == null)
+            fFunctions = FirebaseFunctions.getInstance("europe-west1");
+        Map<String, Object> data = new HashMap<>();
+        data.put("mID", meetingID);
+        data.put("name", name);
+        anyFunction("joinMeeting", data).addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.d("FFunctionsManager:finishedTalking","Task not successful...");
+                task.getException().printStackTrace();
+
+
+                Exception e = task.getException();
+                if (e instanceof FirebaseFunctionsException) {
+                    FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                    FirebaseFunctionsException.Code code = ffe.getCode();
+                    Object details = ffe.getDetails();
+                }
+            }
+            else
+            {
+                Log.d("FFunctionsManager:FinishedTalking","Task succeeded!");
+            }
+        });
+    }
+
 
     /**
      * Joins a hardcoded meeting session and adds the signed in user's email address
      */
-    public void joinSession(String email) {
-
-        //mDatabase.child(sessionID).child("ListOfParticipants").push().setValue(new Participant(email, false));
-        FirebaseFunctionsManager.callJoinMeeting("7",email);
+    public static void joinSession(String email) {
+        callJoinMeeting("7",email);
     }
 
     /**
@@ -32,14 +128,9 @@ public class SessionHandler {
      * represents the existence of a meeting session, as well as the current moderator
      * under the created session.
      */
-    public void createSession(String userMail) {
-        String s = FirebaseFunctionsManager.callNewMeeting(userMail,"7");
+    public static void createSession(String userMail) {
+        String s = callNewMeeting(userMail,"7");
         if(s != null)
             Log.d("Created session", s);
-    }
-
-    public void sendProximityData(String sessionId, long timestamp) {
-        //mDatabase.child(sessionId).child("/QueueRequestData").push().setValue(timestamp);
-        FirebaseFunctionsManager.callEnqueue(sessionId, LoginPageViewModel.getCurrentUser().getEmail(), timestamp);
     }
 }
