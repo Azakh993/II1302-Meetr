113dfa9091a479c92e8de1683254ac0906568845
Azmeer K <azmeer.khawaja@outlook.com>
2023-05-10 03:27:21 +0200

    Reformatting.

--- app/src/main/java/com/group7/meetr/activity/TalkingActivity.java
+++ app/src/main/java/com/group7/meetr/activity/TalkingActivity.java
@@ -1,21 +1,18 @@
 package com.group7.meetr.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.group7.meetr.R;
-import com.group7.meetr.data.remote.QueueHandler;
-import com.group7.meetr.viewmodel.LoginPageViewModel;
-import com.group7.meetr.viewmodel.NewOrJoinMeetingViewModel;
+import com.group7.meetr.viewmodel.TalkingViewModel;
 
 public class TalkingActivity extends AppCompatActivity {
+    private final TalkingViewModel talkingViewModel = new TalkingViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +21,11 @@ public class TalkingActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.buttonFinish);
 
-
-        Vibrator vibr = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                vibr.vibrate(400);
-                Intent intent;
-                //TODO: Make participants activity and replace second variable here.
-                intent = new Intent(TalkingActivity.this, InMeetingActivity.class);
-                QueueHandler.callFinishedTalking(NewOrJoinMeetingViewModel.getCurrentMeetingID());
-                startActivity(intent);
-            }
+        button.setOnClickListener(view -> {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(VibrationEffect.createOneShot(400, 10));
+            talkingViewModel.finishedTalking();
+            finish();
         });
     }
 }
