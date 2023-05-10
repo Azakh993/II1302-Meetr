8f7b843d6bd56de54c07c0191f4901c570267493
Azmeer K <azmeer.khawaja@outlook.com>
2023-05-10 03:22:24 +0200

    Added LiveData to be able to observe when meeting creation / joining is valid and complete.
    Moved in functions and calls from RoleSelectionActivity for better alignment with architecture.

---
+++ app/src/main/java/com/group7/meetr/viewmodel/RoleSelectionViewModel.java
@@ -0,0 +1,78 @@
+package com.group7.meetr.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.group7.meetr.data.model.Meeting;
+import com.group7.meetr.data.model.User;
+import com.group7.meetr.data.remote.SessionHandler;
+
+import java.util.Random;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class RoleSelectionViewModel {
+    private final MutableLiveData<String> userRoleLiveData = new MutableLiveData<>();
+    private String meetingID;
+    private String createOrJoin = null;
+
+    public RoleSelectionViewModel() {
+        meetingIDValidityObserver();
+    }
+
+    private void meetingIDValidityObserver() {
+        SessionHandler.observeMeetingIDValidity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::setMeetingIDValidity);
+    }
+
+    private void setMeetingIDValidity(Boolean meetingIDExists) {
+        if(meetingIDExists && createOrJoin.equals("create")) {
+            userRoleLiveData.setValue("moderator");
+            Meeting.setMeetingID(meetingID);
+            SessionHandler.callNewMeeting(User.getEmail(), meetingID);
+        } else if(!meetingIDExists && createOrJoin.equals("join")) {
+            userRoleLiveData.setValue("participant");
+            Meeting.setMeetingID(meetingID);
+            SessionHandler.callJoinMeeting(meetingID, User.getEmail());
+        } else {
+            userRoleLiveData.setValue("invalid");
+        }
+    }
+
+    private String generateMeetingID() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        int meetingIDLength = 5;
+        for (int i = 0; i < meetingIDLength; i++) {
+            int index = random.nextInt(alphabet.length());
+            char randomChar = alphabet.charAt(index);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+
+    public void joinMeeting(String meetingID) {
+        createOrJoin = "join";
+        this.meetingID = meetingID;
+        checkMeetingID(meetingID);
+    }
+
+    public void createMeeting() {
+        createOrJoin = "create";
+        meetingID = generateMeetingID();
+        checkMeetingID(meetingID);
+    }
+
+    public void checkMeetingID(String meetingID) {
+        SessionHandler.callCheckIfMeetingExists(meetingID);
+    }
+
+    public LiveData<String> getUserRoleLiveData() {
+        return userRoleLiveData;
+    }
+}
