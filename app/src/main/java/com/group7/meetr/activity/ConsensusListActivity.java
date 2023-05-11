7623fac595f1ea508bf8d6fefa482ce7bf274a4b
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-11 11:41:16 +0200

    done with displaying the list of pos/neg consensus with hardcoded lists
    Co-authored-by: peterfann <yanchen.fan0225@gmail.com>

--- app/src/main/java/com/group7/meetr/activity/ConsensusListActivity.java
+++ app/src/main/java/com/group7/meetr/activity/ConsensusListActivity.java
@@ -1,16 +1,51 @@
 package com.group7.meetr.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.group7.meetr.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsensusListActivity extends AppCompatActivity {
 
+    private RecyclerView posListrecycler, negListrecycler;
+    private QueuingListAdapter adapter1, adapter2;
+    private List<String> poslist;
+    private List<String> neglist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consensus_list);
+
+        posListrecycler = findViewById(R.id.posRecyclerViewList);
+        negListrecycler = findViewById(R.id.negRecyclerViewList);
+        posListrecycler.setLayoutManager(new LinearLayoutManager(this));
+        negListrecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        poslist = new ArrayList<>();
+        neglist = new ArrayList<>();
+
+        //poslist.add("hej");
+        //poslist.add("hej");
+        //poslist.add("hej");
+
+        //neglist.add("test");
+        //neglist.add("test");
+        //neglist.add("test");
+
+
+        adapter1 = new QueuingListAdapter(poslist);
+        posListrecycler.setAdapter(adapter1);
+
+        adapter2 = new QueuingListAdapter(neglist);
+        negListrecycler.setAdapter(adapter2);
+
     }
+
+
 }
