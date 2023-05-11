ccb663030d89ad82c88590b15c17cb7f7d0b58c7
Azmeer K <azmeer.khawaja@outlook.com>
2023-05-11 16:32:24 +0200

    Added observer for participants list, to keep track of current consensus status.

--- app/src/main/java/com/group7/meetr/viewmodel/ViewModelUtils.java
+++ app/src/main/java/com/group7/meetr/viewmodel/ViewModelUtils.java
@@ -3,13 +3,16 @@ package com.group7.meetr.viewmodel;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.group7.meetr.data.model.Meeting;
+import com.group7.meetr.data.remote.ConsensusHandler;
 import com.group7.meetr.data.remote.QueueHandler;
 
 import org.jetbrains.annotations.NotNull;
@@ -59,5 +62,40 @@ public class ViewModelUtils {
             }
         };
         endTime.addValueEventListener(endTimeListener);
+    }
+
+    static void participantsConsensusObserver() {
+        String meetingID = Meeting.getMeetingID();
+
+        DatabaseReference endTime = database.getReference("Sessions")
+                .child(meetingID).child("ListOfParticipants");
+
+        ChildEventListener participantsConsensusListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ConsensusHandler.callGetConsensusStances(meetingID);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ConsensusHandler.callGetConsensusStances(meetingID);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("ParticipantsConsensusObserver", error.getDetails());
+            }
+        };
+        endTime.addChildEventListener(participantsConsensusListener);
     }
 }
